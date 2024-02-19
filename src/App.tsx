23692c5b632c4b6b011082d13b7f5eac53e68fe9
Karel Hala <khala@redhat.com>
2024-02-19 11:34:26 +0100

    Add jump links to learning resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.scss';
 import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
 import {
@@ -8,7 +8,17 @@ import {
   QuickStartContextValues,
   filterQuickStarts,
 } from '@patternfly/quickstarts';
-import { Divider, PageSection, StackItem } from '@patternfly/react-core';
+import {
+  Divider,
+  JumpLinks,
+  JumpLinksItem,
+  PageGroup,
+  PageSection,
+  Sidebar,
+  SidebarContent,
+  SidebarPanel,
+  StackItem,
+} from '@patternfly/react-core';
 import CatalogHeader from './components/CatalogHeader';
 import CatalogFilter from './components/CatalogFilter';
 import CatalogSection from './components/CatalogSection';
@@ -26,6 +36,8 @@ export const App = ({ bundle }: { bundle: string }) => {
     loading,
   } = React.useContext<QuickStartContextValues>(QuickStartContext);
   const [contentReady, setContentReady] = useState(false);
+  const [activeItem, setActive] = useState<string>('documentation');
+  const tocRef = React.useRef<HTMLDivElement>(null);
 
   const { documentation, learningPaths, other, quickStarts } = useMemo(() => {
     const filteredQuickStarts = filterQuickStarts(
@@ -73,6 +85,14 @@ export const App = ({ bundle }: { bundle: string }) => {
     chrome?.hideGlobalFilter?.(true);
   }, []);
 
+  const onJumpLinkClick = useCallback(
+    (item: string) => {
+      document.location.href = `${document.location.pathname}#${item}`;
+      setActive(item);
+    },
+    [setActive]
+  );
+
   useEffect(() => {
     fetch(`/api/quickstarts/v1/quickstarts?bundle=${targetBundle}`)
       .then<{ data: { content: QuickStart }[] }>((response) => response.json())
@@ -99,8 +119,8 @@ export const App = ({ bundle }: { bundle: string }) => {
   }
 
   return (
-    <PageSection variant="default" className="pf-u-p-0">
-      <div className="pf-u-p-lg lr-c-catalog__header">
+    <PageGroup>
+      <PageSection className="pf-u-p-lg lr-c-catalog__header">
         <StackItem className="pf-u-mb-md">
           <CatalogHeader />
         </StackItem>
@@ -110,44 +130,90 @@ export const App = ({ bundle }: { bundle: string }) => {
             onSearchInputChange={onSearchInputChange}
           />
         </StackItem>
-      </div>
-      <div className="pf-u-p-lg">
-        <CatalogSection
-          sectionCount={documentation.length}
-          sectionTitle="Documentation"
-          sectionDescription="Technical information for using the service"
-          sectionQuickStarts={documentation}
-          activeQuickStartID={activeQuickStartID}
-          allQuickStartStates={allQuickStartStates}
-        />
-        <Divider className="pf-u-mt-lg pf-u-mb-lg" />
-        <CatalogSection
-          sectionCount={quickStarts.length}
-          sectionTitle="Quick starts"
-          sectionDescription="Step-by-step instructions and tasks"
-          sectionQuickStarts={quickStarts}
-          activeQuickStartID={activeQuickStartID}
-          allQuickStartStates={allQuickStartStates}
-        />
-        <Divider className="pf-u-mt-lg pf-u-mb-lg" />
-        <CatalogSection
-          sectionCount={learningPaths.length}
-          sectionTitle="Learning paths"
-          sectionDescription="Collections of learning materials contributing to a common use case"
-          sectionQuickStarts={learningPaths}
-          activeQuickStartID={activeQuickStartID}
-          allQuickStartStates={allQuickStartStates}
-        />
-        <Divider className="pf-u-mt-lg pf-u-mb-lg" />
-        <CatalogSection
-          sectionCount={other.length}
-          sectionTitle="Other content types"
-          sectionDescription="Tutorials, videos, e-books, and more to help you build your skills"
-          sectionQuickStarts={other}
-          activeQuickStartID={activeQuickStartID}
-          allQuickStartStates={allQuickStartStates}
-        />
-      </div>
-    </PageSection>
+      </PageSection>
+      <PageSection className="pf-u-background-color-200 pf-m-fill">
+      <div className="pf-v5-u-h-100">
+        <Sidebar id="content-wrapper" isPanelRight hasGutter>
+          <SidebarContent
+            id="quick-starts"
+            className="pf-u-background-color-200"
+          >
+            <CatalogSection
+              sectionName="documentation"
+              sectionCount={documentation.length}
+              sectionTitle="Documentation"
+              sectionDescription="Technical information for using the service"
+              sectionQuickStarts={documentation}
+              activeQuickStartID={activeQuickStartID}
+              allQuickStartStates={allQuickStartStates}
+            />
+            <Divider className="pf-u-mt-lg pf-u-mb-lg" />
+            <CatalogSection
+              sectionName="quick-starts"
+              sectionCount={quickStarts.length}
+              sectionTitle="Quick starts"
+              sectionDescription="Step-by-step instructions and tasks"
+              sectionQuickStarts={quickStarts}
+              activeQuickStartID={activeQuickStartID}
+              allQuickStartStates={allQuickStartStates}
+            />
+            <Divider className="pf-u-mt-lg pf-u-mb-lg" />
+            <CatalogSection
+              sectionName="learning-paths"
+              sectionCount={learningPaths.length}
+              sectionTitle="Learning paths"
+              sectionDescription="Collections of learning materials contributing to a common use case"
+              sectionQuickStarts={learningPaths}
+              activeQuickStartID={activeQuickStartID}
+              allQuickStartStates={allQuickStartStates}
+            />
+            <Divider className="pf-u-mt-lg pf-u-mb-lg" />
+            <CatalogSection
+              sectionName="other-content-types"
+              sectionCount={other.length}
+              sectionTitle="Other content types"
+              sectionDescription="Tutorials, videos, e-books, and more to help you build your skills"
+              sectionQuickStarts={other}
+              activeQuickStartID={activeQuickStartID}
+              allQuickStartStates={allQuickStartStates}
+            />
+          </SidebarContent>
+          <SidebarPanel
+            variant="sticky"
+            className="pf-u-background-color-200 pf-u-pl-lg pf-u-pl-0-on-lg"
+          >
+            <div ref={tocRef}>
+              <JumpLinks isVertical label="Jump to section">
+                <JumpLinksItem
+                  onClick={() => onJumpLinkClick('documentation')}
+                  isActive={activeItem === 'documentation'}
+                >
+                  Documentation ({documentation.length})
+                </JumpLinksItem>
+                <JumpLinksItem
+                  onClick={() => onJumpLinkClick('quick-starts')}
+                  isActive={activeItem === 'quick-starts'}
+                >
+                  Quick starts ({quickStarts.length})
+                </JumpLinksItem>
+                <JumpLinksItem
+                  onClick={() => onJumpLinkClick('learning-paths')}
+                  isActive={activeItem === 'learning-paths'}
+                >
+                  Learning paths ({learningPaths.length})
+                </JumpLinksItem>
+                <JumpLinksItem
+                  onClick={() => onJumpLinkClick('other-content-types')}
+                  isActive={activeItem === 'other-content-types'}
+                >
+                  Other content types ({other.length})
+                </JumpLinksItem>
+              </JumpLinks>
+            </div>
+          </SidebarPanel>
+        </Sidebar>
+        </div>
+      </PageSection>
+    </PageGroup>
   );
 };
