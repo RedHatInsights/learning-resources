aae60b5d38dbc2b93096db60f565e29a253d5750
Janet Cobb <jacobb@redhat.com>
2024-05-31 15:24:00 -0400

    Add basic type selection

--- src/Creator.tsx
+++ src/Creator.tsx
@@ -1,8 +1,60 @@
-import React from 'react';
-import { PageGroup } from '@patternfly/react-core';
+import React, { useState } from 'react';
+import { PageGroup, PageSection, Radio, Title } from '@patternfly/react-core';
 
 const Creator = () => {
-  return <PageGroup></PageGroup>;
+  const itemTypes = {
+    documentation: {
+      label: 'Documentation',
+    },
+    quickstart: {
+      label: 'Quickstart',
+    },
+    learningPath: {
+      label: 'Learning path',
+    },
+    other: {
+      label: 'Other',
+    },
+  };
+
+  const [selectedType, setSelectedType] = useState<string | null>(null);
+
+  return (
+    <PageGroup>
+      <PageSection>
+        <Title headingLevel="h1" size="2xl">
+          Add new learning resources
+        </Title>
+        Description
+      </PageSection>
+
+      <PageSection>
+        <div>
+          <h2>Content Type</h2>
+          {Object.entries(itemTypes).map(([name, value]) => {
+            return (
+              <Radio
+                key={name}
+                id={`cr-input-type-${name}`}
+                name={`cr-input-type`}
+                isChecked={name === selectedType}
+                onChange={(_, isChecked) =>
+                  setSelectedType((old) => {
+                    if (isChecked) {
+                      return name;
+                    } else {
+                      return old === name ? null : old;
+                    }
+                  })
+                }
+                label={value.label}
+              ></Radio>
+            );
+          })}
+        </div>
+      </PageSection>
+    </PageGroup>
+  );
 };
 
 export default Creator;
