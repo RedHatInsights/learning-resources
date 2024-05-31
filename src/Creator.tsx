74e857df3ff231e23d30a36af61b6a642f16975e
Janet Cobb <jacobb@redhat.com>
2024-05-31 16:29:57 -0400

    Add form with bundle input

--- src/Creator.tsx
+++ src/Creator.tsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { PropsWithChildren, ReactNode, useMemo, useState } from 'react';
 import {
   FormGroup,
+  FormSelect,
+  FormSelectOption,
+  Grid,
+  GridItem,
   PageGroup,
   PageSection,
   Radio,
   Title,
 } from '@patternfly/react-core';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
 type ItemKind = 'documentation' | 'quickstart';
 
@@ -22,8 +27,79 @@ const itemKindMeta: Record<ItemKind, ItemKindMeta> = {
   },
 };
 
+type CommonItemState = {
+  bundle: string;
+  title: string;
+  description: string;
+};
+
+type ItemFormProps = PropsWithChildren<{
+  commonState: CommonItemState;
+  onChangeCommonState: (newState: CommonItemState) => void;
+}>;
+
+const INVALID_BUNDLE = 'invalid-bundle';
+
+const BundleInput = ({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (newValue: string) => void;
+}) => {
+  const { getAvailableBundles } = useChrome();
+  const bundles = useMemo(() => getAvailableBundles(), []);
+
+  return (
+    <FormGroup label="Bundle">
+      <FormSelect
+        value={value}
+        onChange={(_, value) => onChange(value)}
+        aria-label="Bundle"
+      >
+        <FormSelectOption value={INVALID_BUNDLE} label="--chose one" />
+
+        {...bundles.map((b) => (
+          <FormSelectOption key={b.id} value={b.id} label={b.title} />
+        ))}
+      </FormSelect>
+    </FormGroup>
+  );
+};
+
+const ItemFormElement = ({ children }: { children: ReactNode }) => {
+  return <GridItem span={6}>{children}</GridItem>;
+};
+
+const ItemForm = ({
+  commonState,
+  onChangeCommonState,
+  children,
+}: ItemFormProps) => {
+  return (
+    <Grid>
+      <ItemFormElement>
+        <BundleInput
+          value={commonState.bundle}
+          onChange={(newBundle) =>
+            onChangeCommonState({ ...commonState, bundle: newBundle })
+          }
+        />
+      </ItemFormElement>
+
+      {children}
+    </Grid>
+  );
+};
+
 const Creator = () => {
   const [selectedType, setSelectedType] = useState<ItemKind | null>(null);
+
+  const [commonState, setCommonState] = useState<CommonItemState>({
+    bundle: INVALID_BUNDLE,
+    title: '',
+    description: '',
+  });
 
   return (
     <PageGroup>
@@ -61,6 +137,15 @@ const Creator = () => {
             );
           })}
         </FormGroup>
+      </PageSection>
+
+      <PageSection>
+        <h2>Resource Details</h2>
+
+        <ItemForm
+          commonState={commonState}
+          onChangeCommonState={(state) => setCommonState(state)}
+        ></ItemForm>
       </PageSection>
     </PageGroup>
   );
