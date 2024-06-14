d91651386ae0eec2244225279b37dde140cb5dd4
Janet Cobb <jacobb@redhat.com>
2024-06-14 11:57:19 -0400

    Use a flag to enable creator

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import './App.scss';
 import Viewer from './Viewer';
 import Creator from './Creator';
 import { AppContext, AppNavigationPart } from './AppContext';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
 export const App = ({ bundle }: { bundle: string }) => {
   const [state, setState] = useState<AppNavigationPart>('viewer');
 
+  const {
+    visibilityFunctions: { featureFlag },
+  } = useChrome();
+
+  const creatorEnabled = featureFlag(
+    'platform.learning-resources.creator',
+    true
+  );
+
   return (
-    <AppContext.Provider value={{ onNavigate: (newPart) => setState(newPart) }}>
+    <AppContext.Provider
+      value={{
+        onNavigate: creatorEnabled ? (newPart) => setState(newPart) : undefined,
+      }}
+    >
       {state === 'viewer' ? <Viewer bundle={bundle} /> : <Creator />}
     </AppContext.Provider>
   );
