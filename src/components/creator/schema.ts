54e961c24182f28b18a9afd669839bf60c9ffb4e
Janet Cobb <jacobb@redhat.com>
2024-07-08 14:20:43 -0400

    Match type and details steps of original form

--- src/components/creator/schema.ts
+++ src/components/creator/schema.ts
@@ -1,27 +1,120 @@
 import {
+  ConditionProp,
   Schema,
   componentTypes,
   dataTypes,
   validatorTypes,
 } from '@data-driven-forms/react-form-renderer';
-import { isItemKind, itemKindMeta } from './meta';
+import {
+  ALL_ITEM_KINDS,
+  ItemKind,
+  ItemMeta,
+  isItemKind,
+  itemKindMeta,
+} from './meta';
 import { ChromeAPI } from '@redhat-cloud-services/types';
 
 const REQUIRED = {
   type: validatorTypes.REQUIRED,
 } as const;
 
+function kindMetaCondition(test: (meta: ItemMeta) => boolean): ConditionProp {
+  return {
+    when: 'type',
+    is: (kind: string | undefined) => {
+      return (
+        typeof kind === 'string' && isItemKind(kind) && test(itemKindMeta[kind])
+      );
+    },
+  };
+}
+
+type Bundles = ReturnType<ChromeAPI['getAvailableBundles']>;
+
+function detailsStepName(kind: ItemKind): string {
+  return `step-details-${kind}`;
+}
+
+function makeDetailsStep(kind: ItemKind, bundles: Bundles): object {
+  const meta = itemKindMeta[kind];
+
+  const fields = [];
+
+  fields.push(
+    {
+      component: componentTypes.TEXT_FIELD,
+      name: 'title',
+      label: 'Title',
+      isRequired: true,
+      validate: [REQUIRED],
+    },
+    {
+      component: componentTypes.SELECT,
+      name: 'bundles',
+      label: 'Bundles',
+      simpleValue: true,
+      isMulti: true,
+      options: bundles.map((b) => ({
+        value: b.id,
+        label: `${b.title} (${b.id})`,
+      })),
+    },
+    {
+      component: componentTypes.TEXT_FIELD,
+      name: 'description',
+      label: 'Description',
+      isRequired: true,
+      validate: [REQUIRED],
+    }
+  );
+
+  if (meta.fields.duration) {
+    fields.push({
+      component: componentTypes.TEXT_FIELD,
+      name: 'duration',
+      label: 'Duration',
+      dataType: dataTypes.NUMBER,
+      isRequired: true,
+      validate: [REQUIRED],
+    });
+  }
+
+  if (meta.fields.url) {
+    fields.push({
+      component: componentTypes.TEXT_FIELD,
+      name: 'url',
+      label: 'URL',
+      isRequired: true,
+      validate: [
+        REQUIRED,
+        {
+          type: validatorTypes.URL,
+        },
+      ],
+      condition: kindMetaCondition((meta) => meta.fields.url === true),
+    });
+  }
+
+  return {
+    name: detailsStepName(kind),
+    title: `${meta.displayName} details`,
+    fields: fields,
+  };
+}
+
 export default function makeSchema(chrome: ChromeAPI): Schema {
+  const bundles = chrome.getAvailableBundles();
+
   return {
     fields: [
       {
         component: componentTypes.WIZARD,
-        name: 'wizard-field',
+        name: 'wizard-learning-resource',
         isDynamic: true,
         fields: [
           {
-            name: 'overview',
-            title: 'Overview',
+            name: 'step-type',
+            title: 'Select content type',
             fields: [
               {
                 component: componentTypes.SELECT,
@@ -35,44 +128,15 @@ export default function makeSchema(chrome: ChromeAPI): Schema {
                 isRequired: true,
                 validate: [REQUIRED],
               },
-              {
-                component: componentTypes.TEXT_FIELD,
-                name: 'title',
-                label: 'Title',
-                isRequired: true,
-                validate: [REQUIRED],
-              },
-              {
-                component: componentTypes.SELECT,
-                name: 'bundles',
-                label: 'Bundles',
-                simpleValue: true,
-                isMulti: true,
-                options: chrome.getAvailableBundles().map((b) => ({
-                  value: b.id,
-                  label: `${b.title} (${b.id})`,
-                })),
-              },
-              {
-                component: componentTypes.TEXT_FIELD,
-                name: 'duration',
-                label: 'Duration',
-                isRequired: true,
-                dataType: dataTypes.NUMBER,
-                validate: [REQUIRED],
-                condition: {
-                  when: 'type',
-                  is: (type: string | undefined) => {
-                    return (
-                      typeof type === 'string' &&
-                      isItemKind(type) &&
-                      itemKindMeta[type]?.fields?.duration === true
-                    );
-                  },
-                },
-              },
             ],
+            nextStep: {
+              when: 'type',
+              stepMapper: Object.fromEntries(
+                ALL_ITEM_KINDS.map((kind) => [kind, detailsStepName(kind)])
+              ),
+            },
           },
+          ...ALL_ITEM_KINDS.map((kind) => makeDetailsStep(kind, bundles)),
         ],
       },
     ],
