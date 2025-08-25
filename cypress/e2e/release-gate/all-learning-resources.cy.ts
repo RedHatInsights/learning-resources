0bc3c66f85340ac2d95e842685fd5ca42d9943c2
Darth Tweed <btweed@redhat.com>
2025-08-25 17:01:13 -0500

    Add e2e config

---
+++ cypress/e2e/release-gate/all-learning-resources.cy.ts
@@ -0,0 +1,27 @@
+
+
+describe('All Learning Resources', () => {
+
+  it('appears in the help menu and the link works', () => {});
+  it('appears in search results', () => {});
+  it('loads when navigated to', () => {});
+
+  it('has the appropriate number of items on the tab', () => {
+    // go to all learning resources page
+    // confirm that 50 items appear on the 'All learning resources' tab
+
+  });
+
+  it('performs basic filtering by name', () => {});
+
+  it('filters by product family', () => {});
+
+  it('filters by console-wide services', () => {});
+
+  it('filters by content type', () => {});
+
+  it('filters by use case', () => {});
+
+  it('displays bookmarked resources', () => {});
+
+})
