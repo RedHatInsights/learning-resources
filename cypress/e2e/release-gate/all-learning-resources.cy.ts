6b13e7f9bdb48259b0b762b400437a986c4ec027
Darth Tweed <btweed@redhat.com>
2025-08-27 13:56:59 -0500

    Check for 50 items of content

--- cypress/e2e/release-gate/all-learning-resources.cy.ts
+++ cypress/e2e/release-gate/all-learning-resources.cy.ts
@@ -9,12 +9,23 @@ describe('All Learning Resources', () => {
   });
 
   it('has the appropriate number of items on the tab', () => {
+
+    cy.intercept('GET', '/api/quickstarts/v1/quickstarts?*').as('getQuickstarts');
     // go to All Learning Resources page
     cy.login();
     cy.visit('/learning-resources');
 
-    // confirm that 50 items appear on the 'All learning resources' tab
+    // wait until the page has fully loaded with quickstarts data
+    cy.wait("@getQuickstarts").then((intercept) => {
+      expect(intercept.response.statusCode).to.eq(200)});
 
+    // confirm that 50 items appear on the 'All learning resources' tab
+    // Note: An ID would be a better way to locate the element
+    cy.get('.pf-v6-c-tabs__item-text').contains('All learning resources').then(elem => {
+      console.log(elem.text());
+      const tabText = elem.text();
+      expect(tabText).to.contain('50');
+    })
   });
 
   it('performs basic filtering by name', () => {});
