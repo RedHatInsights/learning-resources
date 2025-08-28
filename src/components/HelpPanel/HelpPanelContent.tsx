e0342bb9b42391ae8e61e8d63f3eafb05c0c2005
Austin Pinkerton <apinkert@redhat.com>
2025-08-28 09:58:27 -0400

    feat: add RH status link to help panel

--- src/components/HelpPanel/HelpPanelContent.tsx
+++ src/components/HelpPanel/HelpPanelContent.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import {
+  Button,
   DrawerActions,
   DrawerCloseButton,
   DrawerHead,
   DrawerPanelBody,
   Title,
 } from '@patternfly/react-core';
+import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 import HelpPanelCustomTabs from './HelpPanelCustomTabs';
 
 const HelpPanelContent = ({ toggleDrawer }: { toggleDrawer: () => void }) => {
   return (
     <>
       <DrawerHead>
-        <Title headingLevel="h2">Help</Title>
+        <Title headingLevel="h2">
+          Help
+          <Button
+            variant="link"
+            onClick={() => window.open('https://status.redhat.com/', '_blank')}
+            isInline
+            className="pf-v6-u-font-size-sm pf-v6-u-font-weight-normal pf-v6-u-ml-md"
+            icon={<ExternalLinkAltIcon />}
+            iconPosition="end"
+          >
+            Red Hat status page
+          </Button>
+        </Title>
         <DrawerActions>
           <DrawerCloseButton onClick={toggleDrawer} />
         </DrawerActions>
