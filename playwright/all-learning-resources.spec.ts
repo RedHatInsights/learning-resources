81ea21a876cc032a23f7577c879fddd1b76edd59
Tweed <btweed@redhat.com>
2025-10-15 21:35:14 -0500

    Add custom message when assertion fails

--- playwright/all-learning-resources.spec.ts
+++ playwright/all-learning-resources.spec.ts
@@ -1,8 +1,8 @@
-import { Page, test } from '@playwright/test';
+import { Page, test, expect } from '@playwright/test';
 
 async function login(page: Page, user: string, password: string): Promise<void> {
   // Fail in a friendly way if the proxy config is not set up correctly
-  expect(page.locator("text=Lockdown")).to_have_count(0)
+  expect(page.locator("text=Lockdown"), 'proxy config incorrect').to_have_count(0)
   await page.getByLabel('Red Hat login').fill(user);
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByLabel('Password').fill(password);
@@ -15,5 +15,5 @@ test('appears in the help menu and the link works', async({page}) => {
     const user = process.env.TEST_USER || 'test';
     const password = process.env.TEST_PASSWORD || 'test';
     await login(page, user, password);
-    expect(page.getByRole('button', {name: 'Add widgets'})).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Add widgets' }), 'dashboard not displayed').toBeVisible();
 });
