387704d957f4a71a5cc49c5c1879d1869fb2770c
Tweed <btweed@redhat.com>
2025-10-15 21:22:15 -0500

    Add login code

--- playwright/all-learning-resources.spec.ts
+++ playwright/all-learning-resources.spec.ts
@@ -1,8 +1,19 @@
-import {test} from '@playwright/test';
+import { Page, test } from '@playwright/test';
+
+async function login(page: Page, user: string, password: string): Promise<void> {
+  await page.getByLabel('Red Hat login').fill(user);
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByLabel('Password').fill(password);
+  await page.getByRole('button', { name: 'Log in' }).click();
+}
 
 
-  test('appears in the help menu and the link works', async({page}) => {
-	  await page.goto('https://console.stage.redhat.com');
-  });
+test('appears in the help menu and the link works', async({page}) => {
+    await page.goto('https://console.stage.redhat.com');
+    const user = process.env.TEST_USER || 'test';
+    const password = process.env.TEST_PASSWORD || 'test';
+    await login(page, user, password);
+    expect(page.getByRole('button', {name: 'Add widgets'})).toBeVisible();
+});
 
 
