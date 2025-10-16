b1f806e963e60b20309e19a253f2ca806a321836
Tweed <btweed@redhat.com>
2025-10-15 19:43:39 -0500

    Playwright test stub

---
+++ playwright/all-learning-resources.cy.ts
@@ -0,0 +1,8 @@
+import {test} from '@playwright/test';
+
+
+  test('appears in the help menu and the link works', async(page) => {
+	  await page.goto('https://console.stage.redhat.com');
+  });
+
+
